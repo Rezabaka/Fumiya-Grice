ebf0cc68a23560b3904ec367eb53400abe1e3464
Rezaxd <167045812+Rezabaka@users.noreply.github.com>
2025-05-01 21:46:38 +0700

    Update settings.js
    Signed-off-by: Rezaxd  <167045812+Rezabaka@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner
+global.owner = ['6285724700472'] //['628','628'] 2 owner
 global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.author = 'Erlan-Team'
+global.botname = 'Fumiya-Grice'
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
